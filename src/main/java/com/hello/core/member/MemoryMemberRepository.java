c19a09aa88ceaf24c1aeadbc68cfae6643eab202
Hanjione <gkswldnj0803@gmail.com>
2023-03-25 22:14:53 +0900

    Initial commit

---
+++ src/main/java/com/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package com.hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();//동시성 이슈해결을 위해서는 concurrent hashmap사용
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
