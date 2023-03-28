7b3ff65387f4737f319bcf9b3fc4b1a684dab043
Hanjione <gkswldnj0803@gmail.com>
2023-03-28 20:00:20 +0900

    test: print all beans

--- src/test/java/com/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/com/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package com.hello.core.beanfind;
 import com.hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextInfoTest {
@@ -16,6 +17,20 @@ public class ApplicationContextInfoTest {
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("beanDefinitionName = " + beanDefinitionName + "object = "+bean);
+        }
+    }
+
+    @Test
+    @DisplayName("print all beans")
+    void findApplicationBean(){
+        String [] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            //ROLE_APPLICATION, ROLE_INFRASTRUCTURE
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){ //내가 짠 것만
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "object = "+bean);
+            }
         }
     }
 }
