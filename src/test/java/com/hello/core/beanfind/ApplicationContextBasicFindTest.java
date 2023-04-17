153b26ea97ba6f7079c8c55a62b7033cf0c0f8ba
Hanjione <gkswldnj0803@gmail.com>
2023-04-17 15:31:02 +0900

    test: print all beans with parent and sub type

---
+++ src/test/java/com/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,46 @@
+package com.hello.core.beanfind;
+
+import com.hello.core.AppConfig;
+import com.hello.core.member.MemberService;
+
+import com.hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 type으로 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean( MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("구체타입으로으로 조회")
+    void findBeanByImpl(){
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+    @Test
+    @DisplayName("빈이름 조회 X")
+    void findBeanFail(){
+//        ac.getBean("XXXX",MemberService.class);
+//        MemberService XXXX = ac.getBean("XXXX", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("XXXX", MemberService.class));
+
+    }
+}
