f89ff69be09f543e65eebf02119b7ec9598bc58f
Hanjione <gkswldnj0803@gmail.com>
2023-06-12 19:34:16 +0900

    remark : remark @Target(ElementType.TYPE)

--- src/test/java/com/hello/core/scan/filter/MyIncludeComponent.java
+++ src/test/java/com/hello/core/scan/filter/MyIncludeComponent.java
@@ -2,7 +2,7 @@ package com.hello.core.scan.filter;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.TYPE)
+@Target(ElementType.TYPE) //ElementType.TYPE은 클래스, 인터페이스, 열거 타입(enum) 선언 시 사용ok
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface MyIncludeComponent {
